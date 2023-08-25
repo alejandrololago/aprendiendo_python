193f5c8c5f0333048a49ffa870da3b07f69a0cbb
alejandrololago <74609496+alejandrololago@users.noreply.github.com>
2023-08-25 17:44:43 -0300

    Subida 2

---
+++ Dia 3/Metodos.py
@@ -0,0 +1,45 @@
+##UPPER
+
+texto = "Este es el texto de prueba"
+resultado = texto.upper()
+print(resultado)
+
+resultado = texto[2].upper()
+print(resultado)
+
+##LOWER
+
+texto = "Este es el texto de prueba"
+resultado = texto.lower()
+print(resultado)
+
+##SPLIT
+
+texto = "Este es el texto de prueba"
+resultado = texto.split() ## Asi por defecto toma como separador a los espacion vacios
+print(resultado)
+
+texto = "Este es el texto de prueba"
+resultado = texto.split("t") ## Asi se cambia el criterio de separacion que toma split
+print(resultado)
+
+##JOIN
+
+a = "Aprender"
+b = "Python"
+c = "es"
+d = "genial"
+e = " ".join([a,b,c,d])
+print(e)
+
+##FIND
+
+texto = "Este es el texto de prueba"
+resultado = texto.find("s") ## Si no encuentra lo buscado arroja un -1
+print(resultado)
+
+##REPLACE
+
+texto = "Este es el texto de prueba"
+resultado = texto.replace("prueba", "Alejandro")
+print(resultado)
---
+++ Dia 3/Propiedades.py
@@ -0,0 +1,16 @@
+nombre = "Carina"
+print(nombre*5)
+
+poema = """Mil pequeños peces blancos
+como si hirviera
+el color del agua"""
+print(poema)
+
+### Conultar si exite determinada palabra
+print("agua" in poema)
+
+print("sol" not in poema)
+
+## Saber el largo de un string
+
+print(len(poema))
