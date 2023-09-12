2570d169fd72baac47c5ef42cd2a37d02300f5fd
alejandrololago <74609496+alejandrololago@users.noreply.github.com>
2023-09-12 09:59:38 -0300

    Subido 12/09/23 09:59 - PC

---
+++ Dia 5/Ayuda.py
@@ -0,0 +1,13 @@
+dic = {'clave1':100,'clave2':500}
+
+a = dic.popitem()
+print(a)
+print(dic)
+
+frase = ",:_#,,,,,,:::____##Pyt%on_ _Total,,,,,,::#"
+print(frase.lstrip(',:%_# '))
+
+frutas = ["mango", "banana", "cereza", "ciruela", "pomelo"]
+frutas.insert(3,'naranja')
+print(frutas)
+
